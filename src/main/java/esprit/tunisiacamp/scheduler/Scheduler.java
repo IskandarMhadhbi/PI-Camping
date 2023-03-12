74ccec10ed6be711cf21182ac754c217096de93e
Aladdinne <aladin.hammouda@esprit.tn>
2023-03-13 00:13:10 +0100

    upd

--- src/main/java/esprit/tunisiacamp/scheduler/Scheduler.java
+++ src/main/java/esprit/tunisiacamp/scheduler/Scheduler.java
@@ -1,5 +1,7 @@
 package esprit.tunisiacamp.scheduler;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 import esprit.tunisiacamp.entities.Autority;
 import esprit.tunisiacamp.entities.Role;
 import esprit.tunisiacamp.entities.User;
@@ -12,6 +14,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -29,21 +32,7 @@ public class Scheduler {
     AutorityRepository autorityRepository;
     @Autowired
     RoleRepository roleRepository;
-    //@Scheduled(cron = "* * * * * *")
-    public void affecterUseraAutority(){
-        List<User> users = (List<User>) userRepository.findAll();
-        List<Autority> autorities = (List<Autority>) autorityRepository.findAll();
-        for(User u : users){
-            Autority au = autorityRepository.getAutority(u);
-                    if(au==null){
-                        Autority a = new Autority();
-                        a.setUserAuth(u);
-                        a.setName(u.getRole1().toString());
-                        autorityRepository.save(a);
-                    }
 
-        }
-    }
     //@Scheduled(cron = "* * * * * * ")
     public void Notification(){
         List<User> users = (List<User>) userRepository.findAll();
@@ -55,18 +44,17 @@ public class Scheduler {
             }
         }
     }
-    //@Scheduled(cron = "* * * * * *")
-    public void affecterUserARole(){
-        List<User> users = (List<User>) userRepository.findAll();
-        for (User u : users){
-            System.out.println("gsfgdf");
-            Role ro = roleRepository.getRole(u.getRole1());
-            System.out.println("gsfgdfcghcvvjbjbjf");
-            if (ro!=null){
-                u.setRole(ro);
-                userRepository.save(u);
-            }
-        }
+    private final String ACCOUNT_SID = "AC40a2bf2c3b42a8ca159c39d88298e173";
+    private final String AUTH_TOKEN = "36c0b81a667077f30f49a8f6f1610678";
+    // private final String FROM_NUMBER = "+14302492629";
+    public void sendSms() {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+21692108297"),
+                new com.twilio.type.PhoneNumber("+14302492629"),
+                "please sign in as soon as possible or we will deactivate your account ").create();
+
+        System.out.println(message.getSid());
     }
 
 
